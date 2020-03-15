fa1a9ab3c51bb92b7c20d0170b4dac144e72f7d8
nakanoh <murry.xxx.7@gmail.com>
2020-03-15 18:07:25 +0900

    tannoa

--- weather.py
+++ weather.py
@@ -2,7 +2,18 @@ import csv
 import pprint
 
 with open('./wether2.csv') as f:
+    # データ読み込み
     reader = csv.reader(f)
+    rows = []
     for row in reader:
-        records.append(row)
-    print(records)
+        rows.append(row)
+
+    # 最低気温日を探す
+    min_tempatures = []
+    for row in rows:
+        min_tempatures.append(row[3])
+        # print(min_tempature)
+    print(min_tempatures)
+    print(min(min_tempatures))
+
+    # print(rows)

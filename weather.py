6dd218eff854c569029d877779b844b2c0ec5781
nakanoh <murry.xxx.7@gmail.com>
2020-03-15 18:17:53 +0900

    交代

--- weather.py
+++ weather.py
@@ -1,18 +1,20 @@
 import csv
 import pprint
 
+# FIXME: CSVリーダーでヘッダーを無視するようにする
 with open('./wether2.csv') as f:
     # データ読み込み
     reader = csv.reader(f)
     rows = []
-    for row in reader:
+    for i, row in enumerate(reader):
+        if i == 0:
+            continue
         rows.append(row)
 
     # 最低気温日を探す
     min_tempatures = []
     for row in rows:
-        min_tempatures.append(row[3])
-        # print(min_tempature)
+        min_tempatures.append(float(row[3]))
     print(min_tempatures)
     print(min(min_tempatures))
 

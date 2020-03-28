e6a291d3698c938a123ce01ac4e18588a8ac7410
nakanoh <murry.xxx.7@gmail.com>
2020-03-28 17:47:32 +0900

    tannoa

--- j1.py
+++ j1.py
@@ -1,5 +1,6 @@
 import csv
-import pprint
+from pprint import pprint
+# import pprint
 
 with open('./j1.csv') as f:
     # データ読み込み
@@ -8,5 +9,12 @@ with open('./j1.csv') as f:
     for i, row in enumerate(reader):
         if i == 0:
             continue
+        # sa = 0
+        # if row[7] > row[8]:
+        #     sa = row[7] - row[8]
+        # else:
+        #     sa = row[8] - row[7]
+        sa = abs(int(row[7]) - int(row[8]))
         rows.append(row)
-    pprint.pprint(rows)
+    pprint(rows)
+

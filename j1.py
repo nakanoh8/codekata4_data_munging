686c8df62bcde46e878b2911735db3ac5783e93a
nakanoh <murry.xxx.7@gmail.com>
2020-03-28 18:20:05 +0900

    nakano end

--- j1.py
+++ j1.py
@@ -2,6 +2,10 @@ import csv
 from pprint import pprint
 # import pprint
 
+team_name = 1
+kachiten = 7
+shitten = 8
+
 with open('./j1.csv') as f:
     # データ読み込み
     reader = csv.reader(f)
@@ -9,9 +13,7 @@ with open('./j1.csv') as f:
     for i, row in enumerate(reader):
         if i == 0:
             continue
-        sa = abs(int(row[7]) - int(row[8]))
         rows.append(row)
-    pprint(sorted(rows, key=lambda row: abs(int(row[7]) - int(row[8]))))
 
-    # key=lambda student: student[2]
-
+    rows_sorted = sorted(rows, key=lambda row: abs(int(row[kachiten]) - int(row[shitten])))
+    print(rows_sorted[0][team_name])

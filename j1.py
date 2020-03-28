e6e96c295b0be09729153bc129a9c1039003eb66
nakanoh <murry.xxx.7@gmail.com>
2020-03-28 17:58:46 +0900

    kaizu

--- j1.py
+++ j1.py
@@ -9,12 +9,8 @@ with open('./j1.csv') as f:
     for i, row in enumerate(reader):
         if i == 0:
             continue
-        # sa = 0
-        # if row[7] > row[8]:
-        #     sa = row[7] - row[8]
-        # else:
-        #     sa = row[8] - row[7]
         sa = abs(int(row[7]) - int(row[8]))
         rows.append(row)
+    pprint(sorted(rows))
     pprint(rows)
 

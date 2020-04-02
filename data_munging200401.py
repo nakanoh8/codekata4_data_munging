e97c3ca89489f48ca724326966e80ccce6168266
nakanoh <murry.xxx.7@gmail.com>
2020-04-02 19:43:22 +0900

    コード修正

--- data_munging200401.py
+++ data_munging200401.py
@@ -3,72 +3,60 @@ import csv
 class DataMungTmpl:
     def __init__(self):
         self.rows = []
-        self.rows_sorted = []
+        self.csv_name = './***.csv'
+        self.target_column = 0
+
+    def sorted_key(self, row):
+        return row
+
     def read_csv_to_rows(self):
-        pass
-    def sort_rows(self):
-        pass
+        with open(self.csv_name) as f:
+            # データ読み込み
+            reader = csv.reader(f)
+            rows = []
+            for i, row in enumerate(reader):
+                # FIXME: CSVリーダーでヘッダーを無視するようにする
+                if i == 0:
+                    continue
+                self.rows.append(row)
+
+    def sorted_rows(self):
+        return sorted(self.rows, key=sorted_key)
+
     def print_first_row_target_column(self):
-        pass
+        print(self.rows[0][self.target_column])
+    
     # Template Method
-    def data_mung_answer():
-        self.read_csv_to_rows()
-        self.sort_rows()
-        self.print_first_row_target_column()
+    def answer_data_mung(self):
+        read_csv_to_rows()
+        sorted_rows()
+        print_first_row_target_column()
 
 
 class DataMungWeather(DataMungTmpl):
-    # get_min_temperature
+    def __init__(self):
+        self.csv_name = './weather.csv'
+        self.target_column = 0  #date
+        self.min_temperature_column = 3
+
     def sorted_key(self, row):
-        min_temperature_column = 3
-        min_temperature = float(row[min_temperature_column])
+        # get_min_temperature
+        min_temperature = float(row[self.min_temperature_column])
         return min_temperature
-    
-    def read_csv_to_rows(self):
-        csv_name = './weather.csv'
-        with open(csv_name) as f:
-            # データ読み込み
-            reader = csv.reader(f)
-            for i, row in enumerate(reader):
-                # FIXME: CSVリーダーでヘッダーを無視するようにする
-                if i == 0:
-                    continue
-                self.rows.append(row)
-
-    def sort_rows(self):
-        self.rows_sorted = sorted(self.rows, key=sorted_key)
-
-    def print_first_row_target_column(self):
-        target_column = 0
-        print(self.rows_sorted[0][target_column])
 
 
 class DataMungJ1(DataMungTmpl):
-    # get_sa
+    def __init__(self):
+        self.csv_name = './j1.csv'
+        self.target_column = 1  #team_name
+        self.kachiten_column = 7
+        self.shitten_column = 8
+
     def sorted_key(self, row):
-        kachiten_column = 7
-        shitten_column = 8
+        # get_sa
         sa = abs(int(row[self.kachiten_column]) - int(row[self.shitten_column]))
         return sa
-    
-    def read_csv_to_rows(self):
-        csv_name = './j1.csv'
-        with open(csv_name) as f:
-            # データ読み込み
-            reader = csv.reader(f)
-            for i, row in enumerate(reader):
-                # FIXME: CSVリーダーでヘッダーを無視するようにする
-                if i == 0:
-                    continue
-                self.rows.append(row)
-
-    def sort_rows(self):
-        self.rows_sorted = sorted(self.rows, key=sorted_key)
-
-    def print_first_row_target_column(self):
-        target_column = 0
-        print(self.rows_sorted[0][target_column])
 
 
-DataMungWeather().data_mung_answer()
-DataMungJ1().data_mung_answer()
+DataMungWeather().answer_data_mung()
+DataMungJ1().answer_data_mung()
